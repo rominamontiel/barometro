e10b22e7135972dc26cf58add82b0319226ee1e3
rominamontiel <romiiemma@gmail.com>
2022-07-02 22:49:46 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { WeatherService } from 'src/services/clima.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'barometro';
+  data:any;
+  press: number | undefined;
+  degs = 0;
+
+  constructor(private apiClima: WeatherService) {}
+
+  ngOnInit() {
+    this.apiClima.getWeather().subscribe(
+      (res) => {
+        console.log(res);
+        this.data = res;        
+        this.setData();
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  setData() {
+    this.press = this.data.main.pressure;
+    if (typeof this.press === 'number') {
+      this.degs = (((((this.press - 930)*100)/120)*180)/100)+2.5;      
+      console.log(this.degs);
+    }
+    
+  }
 }
